d24b5afac9cde370ed35d94079aab8f52918e603
rezacom <reza1880z2@gmail.com>
2020-04-20 14:14:39 +0430

    create new repository

---
+++ index.js
@@ -0,0 +1,25 @@
+//Observer Design Pattern
+class ObserverPattern {
+  constructor() {
+    this.observers = [];
+  }
+  subscribe(observer) {
+    this.observers.push(observer);
+  }
+  notify(change) {
+    this.observers.forEach((observer) => observer(change));
+  }
+}
+
+var observerInstance = new ObserverPattern();
+
+document.getElementById("add").addEventListener('click',() => {
+  let input = document.createElement("input");
+  input.type = "text";
+  document.getElementById("observers").append(input);
+
+  let observer = (value) => (input.value = value);
+  observerInstance.subscribe(observer);
+});
+
+document.getElementById("observable").addEventListener('keyup',(e) => observerInstance.notify(e.target.value));
